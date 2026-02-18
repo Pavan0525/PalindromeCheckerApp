65abfa49f44902faca4cb6e389a848d2de83fc6f
Pavan0525 <pavanaugmar@gmail.com>
2026-02-18 16:37:04 +0530

    Merge remote-tracking branch 'origin/develop' into develop

--- src/PalindromeCheckerAPP.java
+++ src/PalindromeCheckerAPP.java
@@ -1,15 +1,18 @@
 import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Deque;
 
 public class PalindromeCheckerAPP {
 
     public static void main(String[] args) {
-        // UC1: Welcome message
+        // UC1:
         System.out.println("=====================================");
         System.out.println(" Welcome to Palindrome Checker App ");
         System.out.println(" Version: 1.0.0 ");
         System.out.println("=====================================");
 
-        // UC2: Hardcoded palindrome check using StringBuilder reverse
+        // UC2:
         String wordUC2 = "madam";
         String reversedUC2 = new StringBuilder(wordUC2).reverse().toString();
         boolean isPalindromeUC2 = wordUC2.equals(reversedUC2);
@@ -17,7 +20,7 @@ public class PalindromeCheckerAPP {
         System.out.println("Input text : " + wordUC2);
         System.out.println("Is it a palindrome: " + isPalindromeUC2);
 
-        // UC3: Palindrome check using manual reverse with loop
+        // UC3:
         String wordUC3 = "level";
         String reversedUC3 = "";
         for (int i = wordUC3.length() - 1; i >= 0; i--) {
@@ -28,7 +31,7 @@ public class PalindromeCheckerAPP {
         System.out.println("Input text : " + wordUC3);
         System.out.println("Is it a palindrome: " + isPalindromeUC3);
 
-        // UC4: Palindrome check using character array and two-pointer technique
+        // UC4:
         String wordUC4 = "radar";
         char[] chars = wordUC4.toCharArray();
         boolean isPalindromeUC4 = true;
@@ -43,27 +46,67 @@ public class PalindromeCheckerAPP {
             right--;
         }
 
-        System.out.println("Input text : " + wordUC4);
+        System.out.println("Input : " + wordUC4);
         System.out.println("Is it a palindrome: " + isPalindromeUC4);
 
-        // UC5: Palindrome check using Stack
+        // UC5:
         String wordUC5 = "noon";
         Stack<Character> stack = new Stack<>();
-
-        // Push characters into stack
         for (char c : wordUC5.toCharArray()) {
             stack.push(c);
         }
-
-        // Pop characters to build reversed string
         String reversedUC5 = "";
         while (!stack.isEmpty()) {
             reversedUC5 = reversedUC5 + stack.pop();
         }
-
         boolean isPalindromeUC5 = wordUC5.equals(reversedUC5);
 
-        System.out.println("Input  : " + wordUC5);
+        System.out.println("Input : " + wordUC5);
         System.out.println("Is it a palindrome: " + isPalindromeUC5);
+
+        // UC6:
+        String wordUC6 = "civic";
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stackUC6 = new Stack<>();
+
+        for (char c : wordUC6.toCharArray()) {
+            queue.add(c);
+            stackUC6.push(c);
+        }
+
+        boolean isPalindromeUC6 = true;
+        while (!queue.isEmpty()) {
+            char fromQueue = queue.remove();
+            char fromStack = stackUC6.pop();
+            if (fromQueue != fromStack) {
+                isPalindromeUC6 = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + wordUC6);
+        System.out.println("Is it a palindrome: " + isPalindromeUC6);
+
+        // UC7:
+        String wordUC7 = "refer";
+        Deque<Character> deque = new LinkedList<>();
+
+
+        for (char c : wordUC7.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean isPalindromeUC7 = true;
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+            if (front != rear) {
+                isPalindromeUC7 = false;
+                break;
+            }
+        }
+
+        System.out.println("Input: " + wordUC7);
+        System.out.println("Is it a palindrome: " + isPalindromeUC7);
     }
 }
